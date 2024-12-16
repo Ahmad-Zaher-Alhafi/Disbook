dffa205cccfbe1aab842c0f2a3a54bc03ad493cc
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-12-16 12:12:45 +0300

    NEW: Implemented CORS functionality and added it's options to the local variables using dotenv

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const userRouter = require("./routes/userRouter");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: envFile });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGINS,
+};
+app.use(cors(corsOptions));
+
 app.use("/", userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () =>
-  console.log(`Server is listening to https://localhost:${PORT}`)
+  console.log(`Server is listening to http://localhost:${PORT}`)
 );

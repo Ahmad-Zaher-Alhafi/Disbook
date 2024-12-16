2e0caff1784ae779261eff029f2da9b2b970e9bd
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-12-16 12:14:28 +0300

    UPDATE: Replaced the old form submition method by Fetch methode and react useState hook

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,25 +1,91 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const disbookApiUrl = import.meta.env.VITE_Disbook_API_URL;
+
 function Signup() {
+  const [formData, SetFormData] = useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    SetFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSignupSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(disbookApiUrl + "/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).catch((err) => {
+      throw new Error("Could not signup", err);
+    });
+
+    if (!response.ok) {
+      console.error("Could not signup");
+    }
+  };
+
   return (
     <div className="signup">
       <div>Sign up</div>
 
-      <form action="/signup" method="post">
+      <form onSubmit={handleSignupSubmit}>
         <label htmlFor="fullName">Full name</label>
-        <input type="text" id="fullName" required />
+        <input
+          type="text"
+          id="fullName"
+          name="fullName"
+          required
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" required />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          required
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" required />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="confirmPassword">Confirm password</label>
-        <input type="password" id="confirmPassword" required />
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          required
+          onChange={handleInputChange}
+        />
 
         <button>Signup</button>
       </form>

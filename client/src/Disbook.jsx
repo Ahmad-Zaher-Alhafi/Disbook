f3ad5dfbd88442d18769867b74316bedd1919841
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-12-17 12:57:48 +0300

    NEW: User authuntication check before accessing the app, plus to error and loading components

--- client/src/Disbook.jsx
+++ client/src/Disbook.jsx
@@ -1,6 +1,54 @@
+import { useEffect, useState } from "react";
 import "./disbook.css";
+import { useNavigate } from "react-router-dom";
+import * as storage from "./storage";
+import Error from "./components/Error";
+import Loading from "./components/Loading";
+
+const disbookApiUrl = import.meta.env.VITE_Disbook_API_URL;
 
 function Disbook() {
+  const [isAuthorised, setIsAuthorised] = useState();
+  const token = storage.getToken();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/signup");
+      return;
+    }
+
+    const checkAuthuntication = async () => {
+      const response = await fetch(disbookApiUrl + "/users/isAuthorised", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).catch((err) => {
+        console.error("Could not log in", err);
+        throw new Error("");
+      });
+
+      if (!response) {
+        setIsAuthorised(false);
+        navigate("/login");
+        return;
+      }
+
+      setIsAuthorised(true);
+    };
+
+    checkAuthuntication();
+  }, []);
+
+  if (isAuthorised === undefined) {
+    return <Loading></Loading>;
+  }
+
+  if (!isAuthorised) {
+    return <Error></Error>;
+  }
+
   return (
     <div className="disbook">
       <header className="header">
---
+++ client/src/components/Error.jsx
@@ -0,0 +1,9 @@
+function Error() {
+  return (
+    <div className="error">
+      <h1>Unexpected error happened</h1>
+    </div>
+  );
+}
+
+export default Error;

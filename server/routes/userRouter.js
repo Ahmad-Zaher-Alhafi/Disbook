ed0701164cd3b3d413cda4e301e3dc1ca3a34559
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-12-17 18:24:19 +0300

    Removed unused code in the server for now

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,6 @@
 const { Router } = require("express");
 const userController = require("../controllers/userController");
 const passport = require("passport");
-const crypto = require("crypto");
-const axios = require("axios");
 
 const userRouter = Router();
 
@@ -22,31 +20,5 @@ userRouter.get(
     res.json("User is Authorised");
   }
 );
-
-userRouter.get("/users/profile/:identifier", async (req, res) => {
-  const { identifier } = req.params;
-
-  try {
-    const hash = crypto
-      .createHash("sha256")
-      .update(identifier.trim().toLowerCase())
-      .digest("hex");
-
-    const response = await axios.get(
-      `https://api.gravatar.com/v3/profiles/${hash}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.GRAVATAR_API_KEY}`,
-        },
-      }
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(error.response ? error.response.status : 500).json({
-      error: error.message,
-    });
-  }
-});
 
 module.exports = userRouter;

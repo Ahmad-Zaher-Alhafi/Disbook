2d13e6af7f15cfc208c8107e23e6e6dc78927a51
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-12-21 13:52:25 +0300

    UPDATE: Changed the message style and fonts in addition to the font of the message sender

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,22 +1,43 @@
 import defaultUserImage from "../assets/defaultUserImage.png";
 import styles from "../styles/message.module.css";
+import { format, isToday, isYesterday } from "date-fns";
 
 function Message({
   senderName = "User",
   senderImgUrl,
-  sentDate: sentAt = "Today",
+  sentDate,
   content = "Message content",
 }) {
+  function getFormatedDate() {
+    if (isToday(sentDate)) {
+      return `Today at ${format(sentDate, "hh:mm a")}`;
+    } else if (isYesterday(sentDate)) {
+      return `Yesterday at ${format(sentDate, "hh:mm a")}`;
+    } else {
+      return `${format(sentDate, "dd-MM-yyyy")} at ${format(
+        sentDate,
+        "hh:mm a"
+      )}`;
+    }
+  }
+
   return (
-    <div className="message">
-      <img
-        className={styles.img}
-        src={senderImgUrl ? senderImgUrl : defaultUserImage}
-        alt="Sender picture"
-      />
-      <div className="senderName">{senderName}</div>
-      <div className="messageSendDate">{sentAt}</div>
-      <pre className="messageContent">{content}</pre>
+    <div className={styles.message}>
+      <div className="leftSide">
+        <img
+          className={styles.img}
+          src={senderImgUrl ? senderImgUrl : defaultUserImage}
+          alt="Sender picture"
+        />
+      </div>
+
+      <div className={styles.rightSide}>
+        <div className={styles.nameAndDate}>
+          <div className={styles.senderName}>{senderName}</div>
+          <div className={styles.messageSendDate}>{getFormatedDate()}</div>
+        </div>
+        <pre className={styles.messageContent}>{content}</pre>
+      </div>
     </div>
   );
 }
